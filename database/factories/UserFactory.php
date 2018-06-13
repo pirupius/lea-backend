cfe7eb524a84ed18c52120676a985fefd9d741b5
Jared Gerald Ankunda <jaredkage@gmail.com>
2018-06-13 10:25:45 +0000

    some changes to users migration and model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,8 +16,11 @@ use Faker\Generator as Faker;
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        // 'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'phone_number' => $faker->e164PhoneNumber,
+        'district' => $faker->randomElement(['Kampala', 'Mbarara', 'Bushenyi', 'Jinja']),
+        'country' => 'Uganda',
         'remember_token' => str_random(10),
     ];
 });
