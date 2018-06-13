8f34b9a6fa2f01169e07c4d020123ddd0d9fe878
pirupius <piruville@gmail.com>
2018-06-13 18:28:29 +0000

    adds back email to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'password', 'phone_number', 'district', 'country'
+        'name', 'email', 'password', 'phone_number', 'district', 'country'
     ];
 
     /**
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ $factory->define(App\Models\User::class, function ($faker) {
 
     return [
         'name' => $faker->name,
-        // 'email' => $faker->unique()->safeEmail,
+        'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'phone_number' => $faker->e164PhoneNumber,
         'district' => $faker->randomElement(['Kampala', 'Mbarara', 'Bushenyi', 'Jinja']),
