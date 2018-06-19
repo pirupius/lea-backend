4f947f4072ccf3ac6d99143d7d2cb3e57d47bea9
pirupius <piruville@gmail.com>
2018-06-19 12:06:14 +0000

    installs laravel charts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Farmer;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $farmers = Farmer::all();
+
+        return view('home', compact('farmers'));
     }
 }
