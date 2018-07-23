4af405ae94af0f1a08156ab1fdcb1ec41fbb4e64
pirupius <piruville@gmail.com>
2018-07-23 19:45:46 +0000

    fixes cummulative issue with plant tracker

--- app/Http/Controllers/Api/PlantApiController.php
+++ app/Http/Controllers/Api/PlantApiController.php
@@ -11,94 +11,84 @@ class PlantApiController extends Controller
 {
     public function tracker(Request $request)
     {
-        if (isset($request->date)) {
-            $start_date = Carbon::parse($request->date);
-        } else {
-            $start_date = Carbon::now();
+        if (!isset($request->date)) {
+            $request->date = Carbon::now()->toDateString();
         }
 
-        $planting_date = $start_date->toDateString();
+        $planting_date = Carbon::parse($request->date)->toDateString();
         $tracker = array();
 
-        foreach ([[0, 7, 14, 28, 42, 56, 66, 70] as $days) {
+        foreach ([0, 7, 14, 28, 42, 56, 66, 70] as $days) {
             switch ($days) {
                 case 0:
-                    $date = $start_date->addDays($days);
                     array_push($tracker, [
                         'stage'       => 'VE',
                         'days'        => $days,
                         'description' => 'Emergence',
-                        'date'        => $date->format('l jS \\of F Y'),
+                        'date'        => Carbon::parse($request->date)->addDays($days)->format('l jS \\of F Y'),
                     ]);
                     break;
 
                 case 7:
-                    $date = $start_date->addDays($days);
                     array_push($tracker, [
                         'stage'       => 'V2',
                         'days'        => $days,
                         'description' => '2 leaves fully emerged',
-                        'date'        => $date->format('l jS \\of F Y'),
+                        'date'        => Carbon::parse($request->date)->addDays($days)->format('l jS \\of F Y'),
                     ]);
                     break;
 
                 case 14:
-                    $date = $start_date->addDays($days);
                     array_push($tracker, [
                         'stage'       => 'V5',
                         'days'        => $days,
                         'description' => '5 leaves fully emerged, tassel and ear/cob initiation',
-                        'date'        => $date->format('l jS \\of F Y'),
+                        'date'        => Carbon::parse($request->date)->addDays($days)->format('l jS \\of F Y'),
                     ]);
                     break;
 
                 case 28:
-                    $date = $start_date->addDays($days);
                     array_push($tracker, [
                         'stage'       => 'V8',
                         'days'        => $days,
                         'description' => '8 leaves fully emerged',
-                        'date'        => $date->format('l jS \\of F Y'),
+                        'date'        => Carbon::parse($request->date)->addDays($days)->format('l jS \\of F Y'),
                     ]);
                     break;
 
                 case 42:
-                    $date = $start_date->addDays($days);
                     array_push($tracker, [
                         'stage'       => 'V12',
                         'days'        => $days,
                         'description' => '12 leaves',
-                        'date'        => $date->format('l jS \\of F Y'),
+                        'date'        => Carbon::parse($request->date)->addDays($days)->format('l jS \\of F Y'),
                     ]);
                     break;
 
                 case 56:
-                    $date = $start_date->addDays($days);
                     array_push($tracker, [
                         'stage'       => 'VT',
                         'days'        => $days,
                         'description' => '16 leaves',
-                        'date'        => $date->format('l jS \\of F Y'),
+                        'date'        => Carbon::parse($request->date)->addDays($days)->format('l jS \\of F Y'),
                     ]);
                     break;
 
                 case 66:
-                    $date = $start_date->addDays($days);
                     array_push($tracker, [
                         'stage'       => 'R1',
                         'days'        => $days,
                         'description' => '20 leaves, pollination',
-                        'date'        => $date->format('l jS \\of F Y'),
+                        'date'        => Carbon::parse($request->date)->addDays($days)->format('l jS \\of F Y'),
                     ]);
                     break;
 
                 case 70:
-                    $date = $start_date->addDays($days);
                     array_push($tracker, [
                         'stage'       => 'VE',
                         'days'        => 'More than '.$days,
                         'description' => 'Maturity',
-                        'date'        => $date->format('l jS \\of F Y'),
+                        'date'        => Carbon::parse($request->date)->addDays($days)->format('l jS \\of F Y'),
                     ]);
                     break;
             }
